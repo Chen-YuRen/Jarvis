7625712449ba9295cac8b99c23755e21b169c7d3
Chen-YuRen <chenq8299999@gmail.com>
2019-05-27 18:18:12 +0800

    第4次提交

--- practice_1.py
+++ practice_1.py
@@ -2,11 +2,17 @@
 # -*- coding:utf-8 -*-
 
 
-def city_country(name, country):
-    return "\"" + name + ", " + country + "\""
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
 
 
-print(city_country('shanghai', 'China'))
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print('Printing model: ' + current_design)
+    completed_models.append(current_design)
 
-
-
+print("\nThe following models have been printed:")
+for completed_models in completed_models:
+    print(completed_models)
+    
+[x, y]
