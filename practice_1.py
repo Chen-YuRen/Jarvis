7ba638dc8c8808eb65002eb009fdc934bb15f035
Chen-YuRen <chenq8299999@gmail.com>
2019-05-29 00:30:56 +0800

    类--1

--- practice_1.py
+++ practice_1.py
@@ -20,3 +20,6 @@ my_dog = Dog('willie', 6)
 print("My dog's name is " + my_dog.name.title() + ".")
 print("My dog is " + str(my_dog.age) + " years old.")
 
+
+my_dog = Dog('willie', 6)
+print("My dog's name is " + my_dog.mame.title())
