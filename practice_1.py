e2c3529a57d274447a827db900e98c7eb953a18e
Chen-YuRen <chenq8299999@gmail.com>
2019-06-08 15:45:24 +0800

    实验1

--- practice_1.py
+++ practice_1.py
@@ -2,24 +2,11 @@
 # -*- coding:utf-8 -*-
 
 
-class Dog:
-
-    def __int__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def sit(self):
-        print(self.name.title() + " is now sitting.")
-
-    def roll_over(self):
-        print(self.name.title() + " rolled over!")
+def greet_users(names):
+    for name in names:
+        msg = 'Hello, ' + name.title() + '!'
+        print(msg)
 
 
-my_dog = Dog('willie', 6)
-
-print("My dog's name is " + my_dog.name.title() + ".")
-print("My dog is " + str(my_dog.age) + " years old.")
-
-
-my_dog = Dog('willie', 6)
-print("My dog's name is " + my_dog.mame.title())
+usernames = ['hannah', 'try', 'morgot']
+greet_users(usernames)
